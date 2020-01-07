2cca894c3ddc673d2bb7a3e18988c6ec329779c1
BellGuo <45329496+BellGuo@users.noreply.github.com>
2020-01-07 15:27:42 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -5,8 +5,7 @@ export function request(config) {
 
   // 1.创建axios实例
   const instance = axios.create({
-    baseURL : "http://123.207.32.32:8000/api/w1",
-    // baseURL: "http://106.54.54.237:8000/api/w1",
+    baseURL : "test",
     timeout: 5000
   })
 
